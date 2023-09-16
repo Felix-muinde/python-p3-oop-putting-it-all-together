ddaa0740dfa6a94c48e6bc6464226cfcd33e79e9
+ <+>
2023-09-16 16:17:13 +0300

    completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,16 @@
-#!/usr/bin/env python3
+
+ #!/usr/bin/env python3
 
 class Book:
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+if __name__ == "__main__":
+    # You can add code here to test the Book class if needed.
     pass
-    
+   
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,16 @@
+
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "New"
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+if __name__ == "__main__":
+    # You can add code here to test the Shoe class if needed.
     pass
